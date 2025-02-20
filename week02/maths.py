e705655c148c6a24f731084e68e9bba44271b02d
filipavinagre <filipa.vngr@gmail.com>
2025-02-20 05:43:29 +0000

    week02

---
+++ week02/maths.py
@@ -0,0 +1,9 @@
+# maths.py
+# This program calculates 21 - 4 and outputs if 2 is equal to 3
+# Author: Filipa Vinagre
+
+'''
+print (21-4)
+'''
+
+print (2==3)
---
+++ week02/nameAndAge.py
@@ -0,0 +1,8 @@
+# nameAndAge.py
+# This program prints out name and the age of the person
+# Author: Filipa Vinagre
+
+name = input ("Enter your name:")
+age_input = input ("Enter your age: ")
+age = int(age_input)
+print (f'Hello {name}, your age is: {age}')
